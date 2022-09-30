e51b900e3b1943f528566c8a87aaf18395dcc2cc
AleJs <alejscript@gmail.com>
2022-09-30 13:43:28 -0300

    merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> dev
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
-)
-=======
 );
->>>>>>> dev
